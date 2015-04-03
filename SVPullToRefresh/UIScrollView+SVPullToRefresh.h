9021779f233f3d985e1765e0c4cfb35bc3c75f74
Richard Sim <github@devras.com>
2015-04-02 22:52:24 -0700

    Oops

--- SVPullToRefresh/UIScrollView+SVPullToRefresh.h
+++ SVPullToRefresh/UIScrollView+SVPullToRefresh.h
@@ -45,7 +45,6 @@ typedef NS_ENUM(NSUInteger, SVPullToRefreshState) {
 @property (nonatomic, strong, readonly) UILabel *subtitleLabel;
 @property (nonatomic, strong, readwrite) UIColor *activityIndicatorViewColor NS_AVAILABLE_IOS(5_0);
 @property (nonatomic, readwrite) UIActivityIndicatorViewStyle activityIndicatorViewStyle;
-@property (nonatomic, strong, readwrite) UIColor *activityIndicatorViewColor;
 
 @property (nonatomic, readonly) SVPullToRefreshState state;
 @property (nonatomic, readonly) SVPullToRefreshPosition position;
